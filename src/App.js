6513fa5221ea4c10eaaafce81fc8fda558691065
Clair-Hawkes <hawkesj7@gmail.com>
2022-07-19 12:09:13 -0700

    Skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import axios from "axios"
+
 import './App.css';
 
+import Card from './Card';
+
+
+/** App for drawing cards
+ *
+ * State x2:
+ * State: cards: []
+ * State: deckId = integer
+ *
+ * App -> Card
+ */
 function App() {
+  //TODO: cards state
+  // TODO: deckId state
+
+  // TODO: drawCard function
+
+  //TODO: useEffect
+
+
   return (
     <div className="App">
       <header>Draw A Card!</header>
+      <Card></Card>
     </div>
   );
 }
@@ -18,11 +40,12 @@ export default App;
  *
  * App
  * Props:None
- * State: Cards: []
+ * State: cards: []
  * State: deckId = integer
  *
  * function async drawCard(){
  * axios get (http://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=2)
+ * setCards(pile => [...pile,resp.cards[0])]
  * }
  *
  * useEffect(callback(async axios req to set(deckID)),[]) to run only once
@@ -30,11 +53,17 @@ export default App;
  * return (
  * <button onClick=drawCard>
  *
- * {cards.pile.map(card => <div><img src=card.img>)}
- *
- *
+ * {cards.pile.map(card => <Card cardInfo=card)}
  * )
  *
+ *
+ * ________________________________________________
+ *
+ * Card component
+ * State: None
+ * Prop: {cardInfo}
+ * Presentational
+ * return (<div><img src=card.img>)
  *
  *
  *
---
+++ src/Card.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+/** Card component for presenting a card img.
+ *
+ * Props:
+ * - card: {cardInfo}
+ *
+ * App -> Card
+ */
+function Card({card}){
+  return (
+    <header>Card component</header>
+
+    //TODO: Render Card via template
+    // <div><img src={card.img} alt="card"/></div>
+
+
+  )
+}
+
+export default Card;
